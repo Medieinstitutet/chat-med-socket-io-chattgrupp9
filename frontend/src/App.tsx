dcf8d5aba9bb4600bb61a5c4117b85c63460fdd7
Erikli15 <erik.lindner@workmail.com>
2024-03-11 10:21:01 +0100

    chatt lista

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Socket, io } from "socket.io-client";
+import { Chatt } from "./models/Chatt";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [socket, setSocket] = useState<Socket>();
+  const [messages, setMessages] = useState<Chatt[]>([]);
+  const [massageName, setMessageName] = useState("");
+  const [messageText, setMessageText] = useState("");
+  useEffect(() => {
+    if (socket) return;
 
+    const s = io("http://localhost:3000");
+
+    s.on("all_message", (messages: Chatt[]) => {
+      setMessages(messages);
+    });
+    setSocket(s);
+  }, [setSocket, socket]);
+
+  const oneMessage = () => {
+    socket?.emit("one_message", {
+      userName: massageName,
+      message: messageText,
+    });
+  };
+  console.log(messages);
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          type="text"
+          value={massageName}
+          onChange={(e) => setMessageName(e.target.value)}
+          placeholder="Ditt namn"
+        />
+        <br />
+        <textarea
+          onChange={(e) => setMessageText(e.target.value)}
+          placeholder="Chatt medelande"
+        ></textarea>
+        <br />
+        <button onClick={oneMessage}>Skicka</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        {messages.map((msg, i) => {
+          return (
+            <div key={i}>
+              <p>
+                {msg.userName}: {msg.chattMessage}
+              </p>
+            </div>
+          );
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
